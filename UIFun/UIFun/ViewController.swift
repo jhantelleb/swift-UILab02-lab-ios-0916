0abffc8a691ca3e171cfd974efaa7ed0a6ac5fc6
Jhantelle Belleza <jhantellebelleza@Jhantelles-MacBook-Pro.local>
2016-09-01 13:03:47 -0400

    Completed Program

--- UIFun/UIFun/ViewController.swift
+++ UIFun/UIFun/ViewController.swift
@@ -10,18 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
     // TODO: Set up IB outlets
+    
+    @IBOutlet weak var paintBucket: UIView!
+    
+    @IBOutlet weak var firstColor: UISegmentedControl!
+    @IBOutlet weak var secondColor: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: Set the initial paint color to "red"
+        paintBucket.paintColorName = "red"
     }
 
     func mixColors(withFirst first: String, second: String) -> String {
         // TODO: Mix colors and return a string indicating the name of the mixed color
-        // (e.g., "red", "purple", "blue")
-    }
+        // (e.g., "red", "purple", "blue"
+        
+        let lowerFirst = first.lowercaseString
+        let lowerSecond = second.lowercaseString
+        
+        switch lowerFirst{
+        case "red":
+            switch lowerSecond{
+            case "yellow":
+                return "orange"
+            case "blue":
+                return "purple"
+            default:
+                return "red" }
+        case "yellow":
+            switch lowerSecond{
+            case "red":
+                return "orange"
+            case "blue":
+                return "green"
+            default:
+                return "yellow" }
+        case "blue":
+            switch lowerSecond{
+            case "red":
+                return "purple"
+            case "yellow":
+                return "green"
+            default:
+                return "blue" }
+        default:
+            return "red"
+            }
+        }
+    
+
 
     @IBAction func colorSelected(sender: UISegmentedControl) {
         // TODO: Mix each selected color and set the paint color to the mixed color
+        
+        let getFirstColor = firstColor.color.name
+        let getSecondColor = secondColor.color.name
+        
+        let finalColor = mixColors(withFirst: getFirstColor, second: getSecondColor)
+        
+        paintBucket.paintColorName = finalColor
     }
 }
